8f60ee1998349cc6d3e419a97a4a1f7bc559fa21
vanessa <Vane.0192@gmail.com>
2021-08-06 17:33:41 -0500

    my first commit

---
+++ data ingest.py
@@ -0,0 +1,19 @@
+from pyspark.sql import SparkSession
+import os
+from pyspark.sql import dataframe, SparkSession, SQLContext, HiveContext
+from pyspark import SparkContext
+from pyspark.conf import SparkConf
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from pyspark.sql import Row
+
+
+session = SparkSession.builder \
+    .master("local[*]") \
+    .appName("Data-Ingest") \
+    .getOrCreate()
+
+
+df2 = spark.read.option("header",True) \
+    .csv("/tmp/resources/zipcodes.csv")
+
